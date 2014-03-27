bd2ff7c942abee3e405d6fae8ca498bea874710c
Michael Gorsuch <gorsuch@github.com>
2014-03-27 16:49:17 -0500

    Removing these fixes our resource conflicts.

--- manifests/install.pp
+++ manifests/install.pp
@@ -13,11 +13,10 @@ class docker::install {
   case $::osfamily {
     'Debian': {
 
-      ensure_packages(['apt-transport-https', 'cgroup-lite'])
+      ensure_packages(['cgroup-lite'])
       Package['apt-transport-https'] -> Package['docker']
 
       if ($docker::use_upstream_package_source) {
-        include apt
         apt::source { 'docker':
           location          => $docker::package_source_location,
           release           => 'docker',
