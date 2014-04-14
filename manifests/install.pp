f4a500197f4abf1031e0ae9044d5ccbbba00a2c4
Will Farrington <wfarr@github.com>
2014-04-14 11:42:56 -0400

    [docker::install] let's try to do a little bit less

--- manifests/install.pp
+++ manifests/install.pp
@@ -13,7 +13,7 @@ class docker::install {
   case $::osfamily {
     'Debian': {
 
-      ensure_packages(['cgroup-lite'])
+      ensure_resource('package', ['cgroup-lite'])
       Package['apt-transport-https'] -> Package['docker']
 
       if ($docker::use_upstream_package_source) {
